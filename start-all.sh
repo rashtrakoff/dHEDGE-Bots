2b215dc8dd9f39a4aaf79db08954f57ab810fb61
Chinmay Sai Vemuri <55590938+rashtrakoff@users.noreply.github.com>
2025-01-26 23:10:44 +0530

    Modified shell script

--- start-all.sh
+++ start-all.sh
@@ -4,8 +4,12 @@
 pm2 start ecosystem.config.js
 
 # Add a cron job to run the check_liquidatable_positions script daily at midnight
+
+# Ensure the logs directory exists
+mkdir -p ~/Projects/logs
+
 # Define the cron job command
-CRON_JOB="0 0 * * * /home/rashtrakoff/Projects/dHEDGE-Bots/.venv/bin/ape run check_liquidatable_positions --network base:mainnet:alchemy >> /home/rashtrakoff/cronjob.log 2>&1"
+CRON_JOB="0 0 * * * ~/Projects/dHEDGE-Bots/.venv/bin/ape run check_liquidatable_positions --network base:mainnet:alchemy >> ~/Projects/logs/check_liquidatable_positions.log 2>&1"
 
 # Check if the cron job already exists and add it if it doesn't
 (crontab -l 2>/dev/null | grep -F "$CRON_JOB") || (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
